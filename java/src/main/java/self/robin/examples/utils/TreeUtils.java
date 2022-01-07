3e14f26808cd11055d1c333ede435975db67754b
李亚垒（Robin） <liyalei@qiyi.com>
2022-01-07 18:42:44 +0800

    tree util add

--- java/src/main/java/self/robin/examples/utils/TreeUtils.java
+++ java/src/main/java/self/robin/examples/utils/TreeUtils.java
@@ -5,10 +5,7 @@ import org.apache.commons.lang3.reflect.TypeUtils;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.ParameterizedType;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -20,22 +17,26 @@ public class TreeUtils {
      * 注意：保持parentId，primaryKey，firstLevel类型的一致
      *
      * @param collection {@link TreeNode} 子类的集合
-     * @param parentId {@link Function}类型，集合中元素引用的字段（关联字段）
+     * @param parentId   {@link Function}类型，集合中元素引用的字段（关联字段）
      * @param primaryKey {@link Function}类型
      * @param firstLevel 关联字段的第一层的值
      * @return 返回一个树林
      */
-    public static <K, T extends TreeNode<T>> List<T> buildTrees(Collection<T> collection, Function<T, K> parentId, Function<T, K> primaryKey , K firstLevel){
-        //类型一致性检查
-        T t = collection.iterator().next();
-        boolean uniformity = TypeUtils.equals(parentId.apply(t).getClass(), primaryKey.apply(t).getClass())
-                && TypeUtils.equals(parentId.apply(t).getClass(), firstLevel.getClass());
-        if(!uniformity){
-            throw new RuntimeException("parentId, primaryKey, firstLevel 类型不一致");
-        }
+    public static <K, T extends TreeNode<T>> List<T> buildTreesRecurse(Collection<T> collection, Function<T, K> parentId, Function<T, K> primaryKey, K firstLevel) {
+        validData(collection, parentId, primaryKey, firstLevel);
         //分组递归
         Map<K, List<T>> levelGroup = collection.stream().collect(Collectors.groupingBy(parentId));
         return recurseBuildLevel(levelGroup, primaryKey, firstLevel);
+    }
+
+    private static <K, T extends TreeNode<T>> void validData(Collection<T> collection, Function<T, K> parentId, Function<T, K> primaryKey, K firstLevel) {
+        //类型一致性检查
+        T t = collection.iterator().next();
+        boolean uniformity = TypeUtils.equals(parentId.apply(t).getClass(), primaryKey.apply(t).getClass())
+                && (firstLevel == null ? true : TypeUtils.equals(parentId.apply(t).getClass(), firstLevel.getClass()));
+        if (!uniformity) {
+            throw new RuntimeException("parentId, primaryKey, firstLevel 类型不一致");
+        }
     }
 
     /**
@@ -59,8 +60,33 @@ public class TreeUtils {
         return retList;
     }
 
+    public static <K, T extends TreeNode<T>> List<T> buildTreesWhile(Collection<T> collection, Function<T, K> parentId, Function<T, K> primaryKey) {
+        validData(collection, parentId, primaryKey, null);
+        //分组递归
+        Map<K, T> primaryKeyMap = collection.stream().collect(Collectors.toMap(primaryKey, o -> o));
+        Iterator<Map.Entry<K, T>> ite = primaryKeyMap.entrySet().iterator();
+
+        List<T> treeList = new ArrayList<>();
+        while (ite.hasNext()) {
+            Map.Entry<K, T> one = ite.next();
+            K parentKey = parentId.apply(one.getValue());
+            T parent = primaryKeyMap.get(parentKey);
+            if (parent == null || Objects.equals(parentKey, one.getKey())) {
+                treeList.add(one.getValue());
+                continue;
+            }
+            List<T> kids = parent.getKids();
+            if (kids == null || kids.isEmpty()) {
+                kids = new ArrayList<>();
+                parent.setKids(kids);
+            }
+            kids.add(one.getValue());
+        }
+        return treeList;
+    }
+
     @Data
-    public static class TreeNode<T>{
+    public static class TreeNode<T> {
         /**
          * 子树节点
          */
@@ -70,31 +96,34 @@ public class TreeUtils {
     /**
      * 按节点访问所有数据
      */
-    public static <T> void visitNode(List<T> collection, Consumer<T> node) throws Exception{
-        visit(collection, level->{}, node);
+    public static <T> void visitNode(List<T> collection, Consumer<T> node) throws Exception {
+        visit(collection, level -> {
+        }, node);
     }
 
     /**
      * 按层访问所有数据
      */
-    public static <T> void visitLevel(List<T> collection, Consumer<List<T>> level) throws Exception{
-        visit(collection, level, node->{});
+    public static <T> void visitLevel(List<T> collection, Consumer<List<T>> level) throws Exception {
+        visit(collection, level, node -> {
+        });
     }
 
     /**
      * 访问具有树形结构的数据
      * 结构类似：
      * class T{
-     *    List<T> list;
+     * List<T> list;
      * }
+     *
      * @param collection 数据集合
-     * @Param level 按层访问数据
-     * @param node 按节点访数据
+     * @param node       按节点访数据
      * @param <T>
      * @throws Exception 可能发生 IllegalAccessException
+     * @Param level 按层访问数据
      */
-    public static <T> void visit(List<T> collection, Consumer<List<T>> level, Consumer<T> node) throws Exception{
-        if(collection==null){
+    public static <T> void visit(List<T> collection, Consumer<List<T>> level, Consumer<T> node) throws Exception {
+        if (collection == null) {
             return;
         }
         level.accept(collection);
@@ -103,13 +132,13 @@ public class TreeUtils {
             Field[] fields = t.getClass().getDeclaredFields();
             for (Field field : fields) {
                 field.setAccessible(true);
-                if(!List.class.isAssignableFrom(field.getType())){
+                if (!List.class.isAssignableFrom(field.getType())) {
                     continue;
                 }
                 ParameterizedType pt = (ParameterizedType) field.getGenericType();
                 Class<?> clazz = ((Class) pt.getActualTypeArguments()[0]);
-                if(clazz==t.getClass()){
-                    visit((List<T>)field.get(t), level, node);
+                if (clazz == t.getClass()) {
+                    visit((List<T>) field.get(t), level, node);
                 }
             }
         }
